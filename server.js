024d398241bcb8a3c6164bc679476a92a63b7850
Nguyen Duc Dai Nghia <>
2020-11-22 22:04:34 +0700

    Try to run on heroku

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+const distFolder = "/dist/nghia-assignment/browser"
+app.use(express.static(__dirname + distFolder));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + distFolder +  "/index.html"));
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
